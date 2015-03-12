129dbaca4ab2ee6415f8dd27b48885f6a72a9c58
AJ ONeal <coolaj86@gmail.com>
2015-03-12 21:41:32 +0000

    fix #1 answer authoritatively, respond to NS and NAPTR

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ var httpserver = require('./lib/httpserver')
   ;
 
 exports.create = function (server, store, host, port, publicDir) {
-  var app = httpserver.create(store, { host: host, port: port, publicDir: publicDir })
-    ;
+  var app = httpserver.create(store, {
+    host: host
+  , port: port
+  , publicDir: publicDir
+  });
 
   return app;
 };
